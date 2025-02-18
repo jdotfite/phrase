055ccdee96e5c934258f65e2a2aa1029b1374f96
unknown <jdotfite@gmail.com>
2025-02-18 10:42:51 -0500

    fixed hardcoded supabaseurl

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,7 @@
 ﻿import { createClient } from '@supabase/supabase-js'
 import type { Database } from '../types/supabase'
 
-const supabaseUrl = 'https://odtetapubiiqzlfrcfxr.supabase.co'
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || ''
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || ''
 
 export const supabase = createClient<Database>(supabaseUrl, supabaseKey, {
